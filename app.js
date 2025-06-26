e4e121742002166f6893282784231942ba3da0f7
Pratik Rout <pratikofficial2806@gmail.com>
2025-06-26 18:26:20 +0530

    feat: add /demouser route to register a fake user for testing

--- app.js
+++ app.js
@@ -66,6 +66,15 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/demouser", async (req, res) => {
+  let fakeUser = new User({
+    email: "student@gmail.com",
+    username: "delta-student",
+  });
+  let registeredUser = await User.register(fakeUser, "helloworld");
+  res.send(registeredUser);
+});
+
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
 
